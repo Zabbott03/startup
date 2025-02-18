bf768b694d95d9aa43ec1482f8bcf4d500e43ed6
Zach <zacharyabbott@icloud.com>
2025-02-17 17:02:36 -0700

    display the canvas using canvas api and not divs also messed around with it a bit to test out some of the canvas features, still have to make it fit nicely

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,14 +1,69 @@
 import React from 'react';
+import { useRef, useEffect } from 'react';
 
 import './play.css';
 
-function gameBoard() {
-    let board = [];
-    for (let i = 0; i < 255; i++) {
-        board.push(<div class='square'></div>);
+// function gameBoard() {
+//     let board = [];
+//     for (let i = 0; i < 255; i++) {
+//         board.push(<div class='square'></div>);
+//     }
+//     return board;
+// }
+
+function Canvas() {
+    const canvasRef = useRef(null);
+    useEffect(() => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+  
+  
+      const board = {
+        rows: 15,
+        columns: 17,
+        colors: {
+          light: '#abdaab',
+          dark: '#98d098'
+        }
+      }
+      
+      ctx.beginPath();
+      ctx.fillStyle = board.colors.dark;
+      ctx.rect(0, 0, canvas.width, canvas.height);
+      ctx.fill();
+      
+      ctx.beginPath();
+      ctx.fillStyle = board.colors.light;
+      for (let column = 0; column < board.columns; column++) {
+        for (let row = 0; row < board.rows; row++) {
+          if (row % 2 === 0 && column % 2 === 1 || row % 2 === 1 && column % 2 === 0) {
+            ctx.rect(
+              column * canvas.width / board.columns,
+              row * canvas.height / board.rows,
+              canvas.width / board.columns,
+              canvas.height / board.rows
+            );
+          }
+        }
+      }
+      ctx.fill();
+  
+      ctx.beginPath();
+      ctx.fillStyle = "blue";
+      ctx.rect(
+        (5 * canvas.width / board.columns) + 4,
+        (6 * canvas.height / board.rows) + 4,
+        (canvas.width / board.columns) - 8,
+        (canvas.height / board.rows) - 8
+      )
+      ctx.fill();
+    },[]);
+  
+  
+  
+      return <canvas ref={canvasRef} width="643" height="567" />;
     }
-    return board;
-}
+
 
 export function Play() {
   return (
@@ -20,7 +75,8 @@ export function Play() {
                 <h4>P3: 6 points</h4>
             </div>
             <div className="canvas-placeholder">
-                {gameBoard()}
+            <Canvas />
+
             </div>
             <div>
             <button className="start-btn">Start Game</button>
