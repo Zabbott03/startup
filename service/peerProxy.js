0536507660437337c587c2058a5d912e197fbcc2
Zach <zacharyabbott@icloud.com>
2025-04-08 08:23:00 -0600

    removed some console.logs

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -1,13 +1,11 @@
 const { WebSocketServer } = require('ws');
 
 function peerProxy(httpServer) {
-  // Create a websocket object
   const socketServer = new WebSocketServer({ server: httpServer });
 
   socketServer.on('connection', (socket) => {
     socket.isAlive = true;
 
-    // Forward messages to everyone except the sender
     socket.on('message', function message(data) {
       socketServer.clients.forEach((client) => {
         if (client !== socket && client.readyState === WebSocket.OPEN) {
@@ -16,13 +14,11 @@ function peerProxy(httpServer) {
       });
     });
 
-    // Respond to pong messages by marking the connection alive
     socket.on('pong', () => {
       socket.isAlive = true;
     });
   });
 
-  // Periodically send out a ping message to make sure clients are alive
   setInterval(() => {
     socketServer.clients.forEach(function each(client) {
       if (client.isAlive === false) return client.terminate();
